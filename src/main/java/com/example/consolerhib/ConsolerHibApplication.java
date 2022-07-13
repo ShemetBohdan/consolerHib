b7756224d9076d964bc43fa895aa5f991b4f9cb3
Bohdan <shemet.bohdan01@gmail.com>
2022-07-13 21:47:59 +0300

    Initial commit

---
+++ src/main/java/com/example/consolerhib/ConsolerHibApplication.java
@@ -0,0 +1,13 @@
+package com.example.consolerhib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolerHibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolerHibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consolerhib/ConsolerHibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consolerhib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolerHibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
